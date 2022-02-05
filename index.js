88a573ae175805a6d5e9612b5b5cf5093d6b464f
Jesús Monge <jesus_jaml@outlook.com>
2022-02-05 16:44:31 -0500

    Dark and light theme working and persisting using localStorage ✅

--- index.js
+++ index.js
@@ -20,3 +20,25 @@ input.addEventListener('keyup', (event) => {
     window.open(query)
   }
 })
+
+// Dark and light Mode
+const $theme = document.getElementById('theme')
+$theme.onclick = () => {
+  document.body.classList.toggle('dark-theme')
+  if (document.body.classList.contains('dark-theme')) {
+    $theme.textContent = 'Dark theme: on'
+    localStorage.setItem('dark-mode', 'true')
+  } else {
+    $theme.textContent = 'Dark theme: off'
+    localStorage.setItem('dark-mode', 'false')
+  }
+}
+
+// Obtenemos el modo actual por su clave y comprobamos su valor el cual debe ser con cadenas de texto ''
+if (localStorage.getItem('dark-mode') === 'true') {
+  document.body.classList.add('dark-theme')
+  $theme.textContent = 'Dark theme: on'
+} else {
+  document.body.classList.remove('dark-theme')
+  $theme.textContent = 'Dark theme: off'
+}
